10d844bc4cd0c5be7bf57fa9d4937e8ad4c03e1c
Sonam080804 <101492508+Sonam080804@users.noreply.github.com>
2024-01-25 05:11:22 +0000

    container config done

--- 01_basics/Comparison.js
+++ 01_basics/Comparison.js
@@ -1,8 +1,25 @@
-console.log(2 > 1) // Boolean value output => true 
+// console.log(2 > 1) // Boolean value output => true 
 
-console.log("2" > 1) // true 
-console.log("02" > 1)// true
+// console.log("2" > 1) // true 
+// console.log("02" > 1)// true
 
-console.log(null > 0) 
-console.log(null == 0) 
-console.log(null >= 0) 
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+
+
+/* The reason is that check == and comparisons > , < , >= ,
+ <= work differently .  
+ Comparison convert null to a number , treating it as 0 
+ thats why null >= 0 is true and null > 0 is false . */
+
+/* Undefined 
+   Undefined < 0 // false
+   Undefined == 0 // false
+   Undefined >= 0 // false
+*/
+
+// strict check === (check datatypes too)
+
+console.log("2" === 2) // false , bcaz datatypes is not same 
+
