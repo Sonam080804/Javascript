60495e383332de96cf1d4168b57c887a5ff5043b
Sonam080804 <101492508+Sonam080804@users.noreply.github.com>
2024-01-25 05:00:05 +0000

    container config done

---
+++ 01_basics/Comparison.js
@@ -0,0 +1,8 @@
+console.log(2 > 1) // Boolean value output => true 
+
+console.log("2" > 1) // true 
+console.log("02" > 1)// true
+
+console.log(null > 0) 
+console.log(null == 0) 
+console.log(null >= 0) 
