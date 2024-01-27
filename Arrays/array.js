6f8d3e1531397d7d927539c6b9b623adb07354cd
Sonam080804 <101492508+Sonam080804@users.noreply.github.com>
2024-01-27 19:48:52 +0000

    container config done

---
+++ Arrays/array.js
@@ -0,0 +1,10 @@
+const myArray = [0 , 1, 2, 3, 4, 5 , true , "Sonam"]
+
+// Array is an object you may put diffrent data type element under a single name .
+// Resizeable 
+// They are not associative
+//  based indexing 
+// Array make shallow copy (same reference)
+
+
+
